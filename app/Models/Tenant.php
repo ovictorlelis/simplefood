3ab8842a7f9f526ed2596e7e4ebc2802a8e2b97c
Victor Lelis <ovictorlelis@gmail.com>
2024-09-01 16:07:56 -0300

    ajustes migrations

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
 use Stancl\Tenancy\Database\Models\Tenant as Base;
 
-class Tenant extends Base
+class Tenant extends Base implements TenantWithDatabase
 {
-    use HasFactory;
+    use HasFactory, HasDatabase, HasDomains;
 }
